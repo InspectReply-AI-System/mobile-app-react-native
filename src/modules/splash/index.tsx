0dde9e4576532c281369a311706d4cb72a48c8fa
workabhideveloper <kuldeep.singh1@appinventiv.com>
2024-08-11 14:08:17 +0530

    add opacity

--- src/modules/splash/index.tsx
+++ src/modules/splash/index.tsx
@@ -1,19 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import SplashScreen from 'react-native-splash-screen';
 import { Images } from '@inspectreplyai/themes/appImages';
 import Column from '@inspectreplyai/components/general/Column';
 import ImageWrapper from '@inspectreplyai/components/general/Image';
 import { styles } from './styles';
+import { Animated } from 'react-native';
 
 const Splash = () => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
-    SplashScreen.hide();
-  }, []);
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start();
+
+    const timeoutId = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, [opacity]);
 
   return (
     <Column style={styles.container}>
-      <ImageWrapper source={Images.appIcon} style={styles.imageStyle} />
+      <Animated.View style={{ opacity }}>
+        <ImageWrapper source={Images.appIcon} style={styles.imageStyle} />
+      </Animated.View>
     </Column>
   );
 };

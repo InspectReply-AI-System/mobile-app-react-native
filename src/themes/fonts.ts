8c49c96ec09f38e823278b28e98e6d39f2f5e579
workabhideveloper <workabhidev@gmail.com>
2024-08-13 15:17:16 +0530

    feat(IRP):add fonts

--- src/themes/fonts.ts
+++ src/themes/fonts.ts
@@ -1,8 +1,11 @@
 // src/themes/fonts.js
 const fonts = {
-  regular: 'Roboto-Regular',
-  bold: 'Roboto-Bold',
-  italic: 'Roboto-Italic',
+  regular: 'Roboto',
+  thin: 'Roboto Thin',
+  medium: 'Roboto Medium',
+  light: 'Roboto Light',
+  bold: 'Roboto Bold',
+  italic: 'Roboto Italic',
   // Add more font definitions as needed
 };
 

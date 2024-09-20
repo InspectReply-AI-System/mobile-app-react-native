19064670e15fd0fa208d5f917081cde60fb7b1a0
workabhideveloper <workabhidev@gmail.com>
2024-09-20 17:41:20 +0530

    feat(IRP)-add change password module

---
+++ src/modules/profile/password/styles.ts
@@ -0,0 +1,19 @@
+import { colors } from '@inspectreplyai/themes';
+import { normalize } from '@inspectreplyai/utils';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primaryBalck,
+  },
+  inputsContainer: {
+    flex: 1,
+    marginTop: normalize(62),
+    paddingTop: normalize(60),
+    paddingHorizontal: normalize(16),
+    borderTopLeftRadius: normalize(62),
+    borderTopRightRadius: normalize(62),
+    backgroundColor: colors.black27282B,
+  },
+});

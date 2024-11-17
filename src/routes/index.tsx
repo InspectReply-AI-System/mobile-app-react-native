02ebfad56da322f18de739f9ec05b1b908cb7aeb
workabhideveloper <workabhidev@gmail.com>
2024-11-18 00:06:56 +0530

    crashlytics setup complete

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './RootStack';
 import { navigationRef } from '../utils/navigationUtils';
 import ToastMesssge from '@inspectreplyai/components/toast/toastConfig/toast';
 import { colors } from '@inspectreplyai/themes';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 const Navigator = () => {
   const MyTheme = {
@@ -14,9 +15,13 @@ const Navigator = () => {
       background: colors.primaryBlack,
     },
   };
+
+  useEffect(() => {
+    crashlytics().log('App loaded.');
+  }, []);
   return (
     <NavigationContainer theme={MyTheme} ref={navigationRef}>
-      <StatusBar translucent />
+      <StatusBar backgroundColor={colors.primaryBlack} translucent />
       <RootNavigator />
       <ToastMesssge />
     </NavigationContainer>
